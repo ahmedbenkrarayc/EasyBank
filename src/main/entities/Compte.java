24221a32403120ce3a6bff4fca7ed4870d87bf9b
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-09-16 14:27:40 +0100

    EAS-11 Implemented setters & getters for compte class

--- src/main/entities/Compte.java
+++ src/main/entities/Compte.java
@@ -5,7 +5,24 @@ public abstract class Compte {
     protected float solde;
     
     public Compte(String code, float solde) {
+        this.setCode(code);
+        this.setSolde(solde);
+    }
+    
+    //accessors
+    public String getCode() {
+        return this.code;
+    }
+
+    public void setCode(String code) {
         this.code = code;
+    }
+
+    public float getSolde() {
+        return this.solde;
+    }
+
+    public void setSolde(float solde) {
         this.solde = solde;
     }
 }
