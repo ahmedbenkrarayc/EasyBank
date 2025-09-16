37396427848ae33e480fbe808cfb585bd399ae8f
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-09-16 22:49:39 +0100

    EAS-32 created accessors for versement class

--- src/main/entities/Versement.java
+++ src/main/entities/Versement.java
@@ -3,10 +3,19 @@ package main.entities;
 import java.time.LocalDate;
 
 public class Versement extends Operation{
+
 	private String source;
 	
 	public Versement(LocalDate date, float montant, String source) {
 		super(date, montant);
+		this.source = source;
+	}
+	
+	public String getSource() {
+		return source;
+	}
+	
+	public void setSource(String source) {
 		this.source = source;
 	}
 }
