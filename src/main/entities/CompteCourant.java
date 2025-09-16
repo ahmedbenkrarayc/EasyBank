faef1a9f289aefa07f509550f811b5944b016866
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-09-16 15:26:44 +0100

    EAS-15 Implemented retirer() logic in CompteCourant class

--- src/main/entities/CompteCourant.java
+++ src/main/entities/CompteCourant.java
@@ -1,5 +1,49 @@
 package main.entities;
 
-public class CompteCourant {
+public class CompteCourant extends Compte{
+	private float decouvert;
+	
+	public CompteCourant(String code, float solde, float decouvert) {
+		super(code, solde);
+		this.setSolde(decouvert);
+	}
+	
+	//accessors
+	public float getDecouvert() {
+		return this.decouvert;
+	}
+	
+	public void setDecouvert(float decouvert) {
+		this.decouvert = decouvert;
+	}
+	
+	//methods
+	@Override
+	public Boolean retirer(float montant) {
+		float result = this.solde - montant;
+		
+		if(result >= 0) {
+			this.solde = result;
+			return true;
+		}
+		
+		if(-this.decouvert <= result) {		
+			this.solde = result;
+			return true;
+		}
+		
+		return false;
+	}
 
+	@Override
+	public float calculerInteret() {
+		return 0;
+	}
+
+	@Override
+	public String afficherDetails() {
+		return null;
+	}
+	
+	
 }
