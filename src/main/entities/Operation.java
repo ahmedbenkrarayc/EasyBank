3b234b7572a0aefbac8280f175f7acbe9cd13fd7
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-09-16 22:21:13 +0100

    EAS-28 Implemented accessors in operation class

--- src/main/entities/Operation.java
+++ src/main/entities/Operation.java
@@ -10,7 +10,33 @@ public abstract class Operation {
 	
 	public Operation(LocalDate date, float montant) {
 		this.numero = UUID.randomUUID();
+		this.setDate(date);
+		this.setMontant(montant);
+	}
+
+	public UUID getNumero() {
+		return numero;
+	}
+
+	public void setNumero(UUID numero) {
+		this.numero = numero;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
 		this.date = date;
+	}
+
+	public float getMontant() {
+		return montant;
+	}
+
+	public void setMontant(float montant) {
 		this.montant = montant;
 	}
+	
+	
 }
