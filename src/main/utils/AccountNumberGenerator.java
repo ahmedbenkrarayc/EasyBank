94795d6f1cdafc52734e75a5ceec4e1d4cdf756c
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-09-17 11:41:24 +0100

    EAS-40 created AccountNumberGenerator util with generation logic

---
+++ src/main/utils/AccountNumberGenerator.java
@@ -0,0 +1,18 @@
+package main.utils;
+
+import java.util.List;
+
+import main.entities.Compte;
+
+public class AccountNumberGenerator {
+	public static String generate(List<Compte> compte) {
+		String latest = compte.size() > 0 ? compte.get(compte.size() - 1).getCode() : null;
+		if(latest == null) {
+			return "GPT-00000";
+		}
+		
+		int number = Integer.parseInt(latest.split("-")[1]) + 1; 
+		
+		return "GPT-" + String.format("%05d", number);
+	}
+}
